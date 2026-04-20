ef8862d5bdc7e9fd5abe9d12745d42b68d96792c
Son Do <dominhson580@gmail.com>
2026-04-20 17:15:56 +0700

    [be] fee settings

--- be/app/config/settings.py
+++ be/app/config/settings.py
@@ -32,5 +32,11 @@ class Settings(BaseSettings):
     FRONTEND_URL: str
     API_URL: str
 
+    # Fee in USD
+    # TODO: use DB + support multiple tiers
+    CURRENCY: str = "usd"
+    SUBSCRIPTION_FEE: float = 5.0
+    ARTICLE_FEE: float = 1.0
+
 
 settings = Settings()
