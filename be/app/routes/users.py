81d0aeb06a90b21f3e81e9f8419ae7433676a132
Son Do <dominhson580@gmail.com>
2026-08-11 23:04:49 +0700

    [be] user profile api - change response schema

--- be/app/routes/users.py
+++ be/app/routes/users.py
@@ -1,11 +1,21 @@
 from fastapi import APIRouter
 
-from app.schemas.auth import CurrentUser
+from app.schemas.users import GetMeResBody, PublicReader
 from app.dependencies.auth import CurrentUserDep
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@router.get("/me", response_model=CurrentUser)
+@router.get("/me", response_model=GetMeResBody)
 def get_me(current_user: CurrentUserDep):
-    return current_user
+    public_reader = None
+    if current_user.reader:
+        public_reader = PublicReader(
+            **current_user.reader.model_dump(),
+            has_stripe_profile=bool(current_user.reader.stripe_customer_id),
+        )
+    return GetMeResBody(
+        email=current_user.email,
+        is_admin=current_user.is_admin,
+        reader=public_reader,
+    )
